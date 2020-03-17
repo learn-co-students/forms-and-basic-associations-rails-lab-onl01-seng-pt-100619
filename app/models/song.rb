086719f4e2fa353e3052709f8436f15c4c9dc993
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-03-17 09:53:56 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre 
+  has_many :notes
+
+  accepts_nested_attributes_for :notes, reject_if: proc { |attributes| attributes[:content].blank? }
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    artist.try(:name)
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.try(:name)
+  end
+
 end
---
+++ db/migrate/20200317044241_add_artist_name_to_songs.rb
@@ -0,0 +1,5 @@
+class AddArtistNameToSongs < ActiveRecord::Migration[5.0]
+  def change
+    add_column :songs, :artist_name, :string
+  end
+end
