888e21116ed3c7f16d43f83a9a8cc5b0dca91280
steelerx155 <steelerx155@gmail.com>
2020-03-23 13:05:09 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,16 +40,11 @@ end
     end
   end
 
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
+  
 
   private
 
   def song_params
-    params.require(:song).permit(:artist_name, :genre_id, :title, notes_attributes: [:content])
+    params.require(:song).permit(:artist_name, :genre_id, :title, [:content])
   end
 end
